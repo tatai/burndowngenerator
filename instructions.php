04fce3a6a9da4276134cfb2506379e8a687b8374
tatai <fran.naranjo@gmail.com>
2010-04-19 11:20:07 -0700

    Back link in all pages. Create new template with menu and included in main page.

--- instructions.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once(dirname(__FILE__) . '/includes/classes/XTemplate.class.php');
-$xtpl = new XTemplate('instructions.xtpl', dirname(__FILE__) . '/xtpl');
-
-$xtpl->parse('main');
-$xtpl->out('main');
-?>
