9c89bbace2a973d0b0769f4a8bce2e7586b3c8e3
Tatai <fran.naranjo@gmail.com>
2010-05-31 10:53:57 +0200

    Class to manage line color

---
+++ includes/classes/Line/LineColor.class.php
@@ -0,0 +1,65 @@
+<?php
+/* 
+ * Yet another burndown online generator, http://www.burndowngenerator.com
+ * Copyright (C) 2010 Francisco José Naranjo <fran.naranjo@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+class LineColor {
+	private $_red = null;
+	private $_green = null;
+	private $_blue = null;
+	private $_isValid = null;
+
+	/**
+	 * 
+	 * @param $red INumeralSystem value for red
+	 * @param $green INumeralSystem value for green
+	 * @param $blue INumeralSystem value for blue
+	 */
+	public function __construct(INumeralSystem $red, INumeralSystem $green, INumeralSystem $blue) {
+		$this->_isValid = $this->_checkIsValid($red) && $this->_checkIsValid($green) && $this->_checkIsValid($blue);
+		
+		if($this->isValid()) {
+			$this->_red = $red;
+			$this->_green = $green;
+			$this->_blue = $blue;
+		}
+	}
+	
+	public function isValid() {
+		return $this->_isValid;
+	}
+
+	public function red() {
+		return $this->_red->decimal();
+	}
+
+	public function green() {
+		return $this->_green->decimal();
+	}
+
+	public function blue() {
+		return $this->_blue->decimal();
+	}
+
+	private function _checkIsValid(INumeralSystem $number) {
+		$value = $number->decimal();
+		if(is_numeric($value) && $value <= 255 && $value >= 0) {
+			return true;
+		}
+		
+		return false;
+	}
+}
---
+++ tests/Line/LineColorTest.php
@@ -0,0 +1,62 @@
+<?php
+/* 
+ * Yet another burndown online generator, http://www.burndowngenerator.com
+ * Copyright (C) 2010 Francisco José Naranjo <fran.naranjo@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+require_once (dirname(__FILE__) . '/../test_startup.php');
+
+class LineColorTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @test
+	 */
+	public function whenColorIsBiggerThanDecimal255GivesNotValidData() {
+		$colorLine = new LineColor(new Decimal(300), new Decimal(302), new Decimal(304));
+		
+		$this->assertFalse($colorLine->isValid());
+	}
+
+	/**
+	 * @test
+	 */
+	public function whenColorIsSmallerThanDecimal0GivesNotValidData() {
+		$colorLine = new LineColor(new Decimal(-25), new Decimal(-32), new Decimal(-45));
+		
+		$this->assertFalse($colorLine->isValid());
+	}
+
+	/**
+	 * @test
+	 */
+	public function whenColorIsNotNumericReturnsDecimal0() {
+		$colorLine = new LineColor(new Decimal(null), new Decimal(false), new Decimal('er'));
+		
+		$this->assertFalse($colorLine->isValid());
+	}
+
+	/**
+	 * @test
+	 */
+	public function acceptsHexadecimalValuesAndReturnsDecimal() {
+		$colorLine = new LineColor(new Hexadecimal('a'), new Hexadecimal('aa'), new Hexadecimal('ff'));
+		
+		$this->assertEquals(10, $colorLine->red());
+		$this->assertEquals(170, $colorLine->green());
+		$this->assertEquals(255, $colorLine->blue());
+	}
+}
+
+require_once (dirname(__FILE__) . '/../test_shutdown.php');
